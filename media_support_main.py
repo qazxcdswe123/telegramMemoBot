2a82c64c5a6bee1842865bcba25fb88d52112854
Neil-Chen <v009008777@hotmail.com>
2023-01-05 18:19:21 +0800

    rename BOT to BOT_TOKEN

--- media_support_main.py
+++ media_support_main.py
@@ -75,7 +75,7 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater(BOT, use_context=True)
+    updater = Updater(BOT_TOKEN, use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
